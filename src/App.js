ca75c5568ae7dd9147bb4dc1db31165b8929ee43
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2023-06-21 09:26:27 +0300

    Edited

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
              <Box sx={{backgroundColor: '#000'}}>
                  <Navbar />
                  <Routes>
-                     <Route path="/" exact element={<Feed />}/>
+                     <Route path="/YouThube-Clone/" exact element={<Feed />}/>
                      <Route path="/video/:id" element={<VideoDetails />}/>
                      <Route path="/channel/:id" element={<ChannelDetails />}/>
                      <Route path="/search/:searchTerm" element={<SearchFeed />}/>
